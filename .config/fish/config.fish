60569b45b4fa6d65307f0f58f3aacc062b5982b6
Jajafarov <jafarovislam95@gmail.com>
2019-09-13 12:55:54 +0200

    Added env variables to fish so that scratchterm behaves regularly

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,17 @@ if status --is-interactive
     abbr --add --global fd fd -I
 end
 
+set -x XDG_CONFIG_HOME $HOME/.config
+set -x XDG_CACHE_HOME $HOME/.cache
+set -x XDG_DATA_HOME $HOME/.local/share
+set -x PARALLEL_HOME $XDG_CONFIG_HOME/parallel
+set -x PASSWORD_STORE_DIR $XDG_DATA_HOME/pass
+set -x GNUPGHOME $XDG_DATA_HOME/gnupg
+set -x WGETRC $XDG_CONFIG_HOME/wgetrc
+set -x CARGO_HOME $XDG_DATA_HOME/cargo
+set -x LESSKEY $XDG_CONFIG_HOME/less/lesskey
+set -x LESSHISTFILE $XDG_CACHE_HOME/less/history
+
 alias cp="cp -i"                          # confirm before overwriting something
 alias df="df -h"                          # human-readable sizes
 alias free="free -m"                      # show sizes in MB
